eda021ff78d567cde5ddd35828d87bbe5acc1381
MrSwed <webmaster@sdcollection.com>
2016-11-08 12:09:36 +0300

    json config

--- admin.php
+++ admin.php
@@ -1,10 +1,19 @@
 <?
 
+if (!$config = file_get_contents("config.json")) {
+	die("config.json not found");
+}
+
+if (!$config = json_decode($config,true)) {
+	die("config.json has no config JSON");
+}
+
+
+
 echo "hello world";
 
+print "<pre>";
+print_r($config);
 
-$db = sqlite_open("db.sqlite");
-
-sqlite_close($db);
 
 ?>
