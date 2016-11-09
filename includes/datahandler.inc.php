b1761b67adc8f7560855a2d7ee6378f7c4da10d0
MrSwed <webmaster@sdcollection.com>
2016-11-09 07:27:31 +0300

    start datahandler class

---
+++ includes/datahandler.inc.php
@@ -0,0 +1,21 @@
+<?
+/*
+ * DataHandler class
+ * */
+class DataHandler {
+	var $db; // db object
+	var $config= null;
+	var $result;
+	var $sql;
+	var $debug;
+	/**
+		* datahandler constructor
+		*
+		* @return DataHandler
+		*/
+	function __construct() {
+
+	}
+
+}
+?>
