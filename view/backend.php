12b2f7d13a9d849fde37e5c0d0e676387f4069fd
MrSwed <webmaster@sdcollection.com>
2016-11-17 00:01:34 +0300

    start backend

---
+++ view/backend.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Форма заказа (test)</title>
+	<link rel="stylesheet" href="/styles.css">
+	<script src="/jquery-1.9.0-min.js"></script>
+	<script src="/js/backlogic.js"></script>
+</head>
+<body>
+<pre>
+	<?
+	print_r($form);
+	?>
+</pre>
+</body>
+</html>
